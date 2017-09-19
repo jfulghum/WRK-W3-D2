4abe046ce0523a1840b281035548563caf5bb61f
App Academy Student <johanna.fulghum@gmail.com>
2017-09-19 12:17:16 -0400

    complete phase 1

---
+++ AA-Questions/import_db.sql
@@ -0,0 +1,86 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL
+
+);
+
+DROP TABLE IF EXISTS questions;
+
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+-- join table exists to join tables together
+DROP TABLE IF EXISTS question_follows;
+
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+
+  q_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY(q_id) REFERENCES questions(id),
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS replies;
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  body TEXT NOT NULL,
+  q_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  reply_parent_id INTEGER,
+
+  FOREIGN KEY(q_id) REFERENCES questions(id),
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(reply_parent_id) REFERENCES replies(id)
+);
+
+DROP TABLE IF EXISTS question_likes;
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  q_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  likes BOOLEAN,
+
+
+  FOREIGN KEY(q_id) REFERENCES questions(id),
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Johanna', 'Fulghum'),
+  ('Oguzhan', 'Cakmak'),
+  ('Patrick', 'Fulghum');
+
+INSERT INTO
+  questions (title, body, user_id)
+VALUES
+  ('What planet is this?', 'Help me please', 1),
+  ('SQL Help', 'I couldn''t solve my problem Help!', 2);
+
+INSERT INTO
+  replies (body, q_id, user_id, reply_parent_id)
+VALUES
+  ('Earth', 1, 1, NULL),
+  ('Looks like Mars to me!! Loser', 1, 2, 1),
+  ('No help here man.', 2, 1, NULL),
+  ('I can help! SQL is great', 2, 3, 3);
+
+INSERT INTO
+  question_likes (q_id, user_id, likes)
+VALUES
+  (1, 2, 1),
+  (2, 3, 1);
---
+++ AA-Questions/questions.rb
@@ -0,0 +1,92 @@
+require 'sqlite3'
+require 'singleton'
+
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class User
+  attr_accessor :fname, :lname
+
+end
+
+
+class Question
+  attr_accessor :title, :body, :user_id
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM plays")
+    data.map { |datum| Play.new(datum) }
+  end
+
+  def self.find_by_title(title)
+    play = PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        title = ?
+    SQL
+    return nil unless play.length > 0
+
+    Play.new(play.first) # play is stored in an array!
+  end
+
+  def self.find_by_playwright(name)
+    playwright = Playwright.find_by_name(name)
+    raise "#{name} not found in DB" unless playwright
+
+    plays = PlayDBConnection.instance.execute(<<-SQL, playwright.id)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+
+    plays.map { |play| Play.new(play) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @year = options['year']
+    @playwright_id = options['playwright_id']
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @title, @year, @playwright_id)
+      INSERT INTO
+        plays (title, year, playwright_id)
+      VALUES
+        (?, ?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @title, @year, @playwright_id, @id)
+      UPDATE
+        plays
+      SET
+        title = ?, year = ?, playwright_id = ?
+      WHERE
+        id = ?
+    SQL
+  end
+end
+
+class Reply
+  attr_accessor :body, :q_id, :user_id, :reply_parent_id
+
+end
