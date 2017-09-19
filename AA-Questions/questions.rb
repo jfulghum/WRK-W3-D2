d05d511db034ad0c20694d45995ee7102312d0d7
App Academy Student <johanna.fulghum@gmail.com>
2017-09-19 14:00:52 -0400

    Implement Class methods

--- AA-Questions/questions.rb
+++ AA-Questions/questions.rb
@@ -13,80 +13,94 @@ end
 
 class User
   attr_accessor :fname, :lname
+  attr_reader :id
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+      *
+      FROM
+      users
+      WHERE
+      id = ?
+    SQL
+    p data
+    User.new(data.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options["fname"]
+    @lname = options["lname"]
+  end
 
 end
 
 
 class Question
   attr_accessor :title, :body, :user_id
+  attr_reader :id
 
-  def self.all
-    data = PlayDBConnection.instance.execute("SELECT * FROM plays")
-    data.map { |datum| Play.new(datum) }
-  end
-
-  def self.find_by_title(title)
-    play = PlayDBConnection.instance.execute(<<-SQL, title)
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
       SELECT
-        *
+      *
       FROM
-        plays
+      questions
       WHERE
-        title = ?
+      id = ?
     SQL
-    return nil unless play.length > 0
-
-    Play.new(play.first) # play is stored in an array!
-  end
-
-  def self.find_by_playwright(name)
-    playwright = Playwright.find_by_name(name)
-    raise "#{name} not found in DB" unless playwright
-
-    plays = PlayDBConnection.instance.execute(<<-SQL, playwright.id)
-      SELECT
-        *
-      FROM
-        plays
-      WHERE
-        playwright_id = ?
-    SQL
-
-    plays.map { |play| Play.new(play) }
+    p data
+    Question.new(data.first)
   end
 
   def initialize(options)
     @id = options['id']
-    @title = options['title']
-    @year = options['year']
-    @playwright_id = options['playwright_id']
+    @title = options["title"]
+    @body = options["body"]
+    @user_id = options["user_id"]
   end
 
-  def create
-    raise "#{self} already in database" if @id
-    PlayDBConnection.instance.execute(<<-SQL, @title, @year, @playwright_id)
-      INSERT INTO
-        plays (title, year, playwright_id)
-      VALUES
-        (?, ?, ?)
-    SQL
-    @id = PlayDBConnection.instance.last_insert_row_id
-  end
+  # def self.all
+  #   data = PlayDBConnection.instance.execute("SELECT * FROM plays")
+  #   data.map { |datum| Play.new(datum) }
+  # end
 
-  def update
-    raise "#{self} not in database" unless @id
-    PlayDBConnection.instance.execute(<<-SQL, @title, @year, @playwright_id, @id)
-      UPDATE
-        plays
-      SET
-        title = ?, year = ?, playwright_id = ?
+end
+
+class QuestionFollow
+  attr_accessor :q_id, :user_id
+  attr_reader :id
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+      *
+      FROM
+      question_follows
       WHERE
-        id = ?
+      id = ?
     SQL
+    p data
+    Question.new(data.first)
   end
+
+  def initialize(options)
+    @id = options['id']
+    @q_id = options["q_id"]
+    @user_id = options["user_id"]
+  end
+
 end
 
 class Reply
   attr_accessor :body, :q_id, :user_id, :reply_parent_id
+  attr_reader :id
+
+end
+
+class QuestionLikes
+  attr_accessor :q_id, :user_id, :likes
+  attr_reader :id
 
 end
